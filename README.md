1b9bbb44e90133b550388a87ddb823c23ed5c5d8
Moritz Mähr <maehr@users.noreply.github.com>
2023-10-26 11:29:26 +0200

    initial alpha release (#1)
    - feat: Introduced a simple topic modeling app using Latent Dirichlet Allocation (LDA) for discovering hidden topics in text documents.
    - feat: Added a variety of new UI components including a number input widget, a video playback feature, a slider component, a camera input component, a button widget, a progress bar, a radio group, and a date input widget.
    - fix: Updated the contact method for reporting abusive behavior and security issues.
    - fix: Enhanced the Python editor with automatic code fixes on save.
    - fix: Updated the .gitignore and .prettierignore files for better project management.
    - docs: Updated the README, installation instructions, usage, development, support, roadmap, authors, and license sections.
    - docs: Updated the release notes and the links in them.
    - style: Made numerous styling and layout adjustments across the project.
    - chore: Updated the project's license information and author credits.

--- README.md
+++ README.md
@@ -1,116 +1,66 @@
-# GitHub template for small projects
+# simple-topic-modeling
 
-This is a GitHub template designed for small projects that is independent of the programming language used. It uses various GitHub features to manage issues and pull requests, while enforcing best practices for small projects.
+This app is a simple topic modeling tool that uses Latent Dirichlet Allocation (LDA) to discover hidden topics in a corpus of text documents. It's designed for researchers, data scientists, and anyone interested in text analytics. Its basic version runs in the browser.
 
-[![GitHub issues](https://img.shields.io/github/issues/maehr/github-template.svg)](https://github.com/maehr/github-template/issues)
-[![GitHub forks](https://img.shields.io/github/forks/maehr/github-template.svg)](https://github.com/maehr/github-template/network)
-[![GitHub stars](https://img.shields.io/github/stars/maehr/github-template.svg)](https://github.com/maehr/github-template/stargazers)
-[![GitHub license](https://img.shields.io/github/license/maehr/github-template.svg)](https://github.com/maehr/github-template/blob/main/LICENSE.md)
+[![GitHub issues](https://img.shields.io/github/issues/maehr/simple-topic-modeling.svg)](https://github.com/maehr/simple-topic-modeling/issues)
+[![GitHub forks](https://img.shields.io/github/forks/maehr/simple-topic-modeling.svg)](https://github.com/maehr/simple-topic-modeling/network)
+[![GitHub stars](https://img.shields.io/github/stars/maehr/simple-topic-modeling.svg)](https://github.com/maehr/simple-topic-modeling/stargazers)
+[![GitHub license](https://img.shields.io/github/license/maehr/simple-topic-modeling.svg)](https://github.com/maehr/simple-topic-modeling/blob/main/LICENSE.md)
 
-## Why use a template (even for small projects)
+## Demo
 
-- Write better [documentation](#documentation) for you and the community
-- Write more [consistent](#consistency) code and foster collaboration
-- Increase [security](#security)
-- Follow recognized [ethical principles](#ethics)
-- Promote [Free and Open-source software](#foss)
-
-## How this template helps you
-
-### Documentation
-
-- [README.md](README.md) according to [www.makeareadme.com](https://www.makeareadme.com/)
-- [CHANGELOG.md](CHANGELOG.md) according to [keepachangelog.com](https://keepachangelog.com/)
-- Automated [CHANGELOG.md](CHANGELOG.md) via [git-cliff](https://github.com/orhun/git-cliff)
-- [package.json](package.json) according to [npm docs](https://docs.npmjs.com/cli/v7/configuring-npm/package-json)
-- Accessible documentation via [gh-pages](https://help.github.com/en/articles/configuring-a-publishing-source-for-github-pages)
-
-### Consistency
-
-- Consistent formatting via [Prettier](https://prettier.io/)
-- Consistent commit messages according to [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) via [husky](https://github.com/typicode/husky)
-- Consistent versioning via [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
-- Consistent [fork and pull](https://gist.github.com/Chaser324/ce0505fbed06b947d962) workflow via [GitHub branch protection](https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/managing-a-branch-protection-rule)
-- Consistent issues via [issue templates](https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/configuring-issue-templates-for-your-repository)
-- Consistent labels for pull requests via [labeler](https://github.com/actions/labeler)
-
-### Security
-
-- [SECURITY.md](SECURITY.md) according to [GitHub](https://docs.github.com/en/code-security/getting-started/adding-a-security-policy-to-your-repository)
-- [GitHub security alerts](https://github.blog/2017-11-16-introducing-security-alerts-on-github/)
-- Integrity via [GitHub branch protection](https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/defining-the-mergeability-of-pull-requests/managing-a-branch-protection-rule)
-
-### Ethics
-
-- [CODE_OF_CONDUCT.md](CODE_OF_CONDUCT.md) according to [Contributor Covenant](https://www.contributor-covenant.org/)
-- Friendly first interactions via [greetings](https://github.com/actions/starter-workflows/blob/main/automation/greetings.yml)
-
-### FOSS
-
-- [LICENSE.md](LICENSE.md) according to [choosealicense.com](https://choosealicense.com/)
+You can try out the app [here](https://maehr.github.io/simple-topic-modeling/). If you want to visualize the topics with the more advanced [PyLDAvis](https://github.com/bmabey/pyLDAvis) library, you need to run the app locally.
 
 ## Installation
 
-To initialize the template, [generate](https://github.com/maehr/github-template/generate) or [clone](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository) the repository to your local machine and follow this guide.
+This section provides instructions on how to install the dependencies required to run the app locally. Make sure you have [Python](https://www.python.org/downloads/) 3.11 or higher and [Node.js](https://nodejs.org/en/download/) installed before proceeding.
 
-1. Install [Node.js](https://nodejs.org/en/) and run the following commands in the root directory of the repository:
+Use the package manager [poetry](https://python-poetry.org/docs/) to install all Python dependencies. Run the following command in your terminal:
+
+```bash
+poetry install
+```
+
+Use the package manager [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) to install more dev dependencies like [prettier](https://prettier.io/). Run the following command in your terminal:
 
 ```bash
 npm install
-npm run prepare
 ```
-
-2. Complete the following checklist of tasks to customize the template for your project:
-
-- [ ] enable [GitHub security alerts](https://github.blog/2017-11-16-introducing-security-alerts-on-github/)
-- [ ] [protect](https://help.github.com/en/articles/configuring-protected-branches) the main branch to enforce a [fork and pull](https://gist.github.com/Chaser324/ce0505fbed06b947d962) workflow
-- [ ] search and replace `FULLNAME`, `USERNAME`, `REPO_NAME`, `SHORT_DESCRIPTION` `[INSERT CONTACT METHOD]` in [CODE_OF_CONDUCT.md](CODE_OF_CONDUCT.md), [package.json](package.json), [README.md](README.md), [SECURITY.md](SECURITY.md), [config.yml](.github/ISSUE_TEMPLATE/config.yml)
-- [ ] add favicons to `./`, e.g. via [favicon.io](https://favicon.io/)
-- [ ] search for `TODO` in the project (mostly documentation) and fix it
-- [ ] delete [README.md](README.md)
-- [ ] rename [README.template.md](README.template.md) to [README.md](README.md)
-- [ ] run `npm run format` to format all files
-- [ ] run `npm run commit` to commit all changes
-- [ ] run `npm run changelog` and include the output in [CHANGELOG.md](CHANGELOG.md)
-- [ ] enable [gh-pages](https://help.github.com/en/articles/configuring-a-publishing-source-for-github-pages)
 
 ## Usage
 
-Check that all files are properly formatted.
+To run the app locally, use the following command.
 
 ```bash
-npm run check
+poetry run streamlit run src/app.py
 ```
 
-Format all files.
+## Development
+
+If you want to contribute to this project, you can use the following commands to test the browser version of the app.
 
 ```bash
-npm run format
+python3 -m http.server 8000 --directory .
 ```
 
-Run the wizard to write meaningful commit messages.
+To format the code, use the following commands.
 
 ```bash
-npm run commit
-```
-
-Run the wizard to create a CHANGELOG.md.
-
-```bash
-npm run changelog
+poetry run ruff format
+npm format
 ```
 
 ## Support
 
 This project is maintained by [@maehr](https://github.com/maehr). Please understand that we won't be able to provide individual support via email. We also believe that help is much more valuable if it's shared publicly, so that more people can benefit from it.
 
-| Type                                  | Platforms                                                                  |
-| ------------------------------------- | -------------------------------------------------------------------------- |
-| 🚨 **Bug Reports**                    | [GitHub Issue Tracker](https://github.com/maehr/github-template/issues)    |
-| 📚 **Docs Issue**                     | [GitHub Issue Tracker](https://github.com/maehr/github-template/issues)    |
-| 🎁 **Feature Requests**               | [GitHub Issue Tracker](https://github.com/maehr/github-template/issues)    |
-| 🛡 **Report a security vulnerability** | See [SECURITY.md](SECURITY.md)                                             |
-| 💬 **General Questions**              | [GitHub Discussions](https://github.com/maehr/github-template/discussions) |
+| Type                                   | Platforms                                                                        |
+| -------------------------------------- | -------------------------------------------------------------------------------- |
+| 🚨 **Bug Reports**                     | [GitHub Issue Tracker](https://github.com/maehr/simple-topic-modeling/issues)    |
+| 📚 **Docs Issue**                      | [GitHub Issue Tracker](https://github.com/maehr/simple-topic-modeling/issues)    |
+| 🎁 **Feature Requests**                | [GitHub Issue Tracker](https://github.com/maehr/simple-topic-modeling/issues)    |
+| 🛡 **Report a security vulnerability** | See [SECURITY.md](SECURITY.md)                                                   |
+| 💬 **General Questions**               | [GitHub Discussions](https://github.com/maehr/simple-topic-modeling/discussions) |
 
 ## Roadmap
 
@@ -120,11 +70,15 @@ No changes are currently planned.
 
 Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
 
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/maehr/simple-topic-modeling/tags).
+
 ## Authors and acknowledgment
 
 - **Moritz Mähr** - _Initial work_ - [maehr](https://github.com/maehr)
 
-See also the list of [contributors](https://github.com/maehr/github-template/graphs/contributors) who participated in this project.
+See also the list of [contributors](https://github.com/maehr/simple-topic-modeling/graphs/contributors) who participated in this project.
 
 ## License
 
---
+++ src/app.py
@@ -0,0 +1,271 @@
+import base64
+import platform
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from utils.stopwords import german, french, spanish
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from io import StringIO, BytesIO
+
+if not platform.system() == "Emscripten":
+    import pyLDAvis
+    import pyLDAvis.lda_model
+
+# Global variables for stop words and n-gram options
+STOP_WORDS = {
+    "english": "english",
+    "german": german,
+    "french": french,
+    "spanish": spanish,
+}
+
+NGRAM_OPTIONS = {
+    "unigram": (1, 1),
+    "unigram + bigram": (1, 2),
+    "unigram + bigram + trigram": (1, 3),
+    "bigram": (2, 2),
+    "trigram": (3, 3),
+}
+
+
+def fig_to_base64(fig):
+    """
+    Convert a Matplotlib figure to Base64 encoding.
+
+    Parameters:
+        fig (matplotlib.figure.Figure): The figure to convert.
+
+    Returns:
+        str: Base64 encoded string of the figure.
+    """
+    img = BytesIO()
+    fig.savefig(img, format="png")
+    img.seek(0)
+    return base64.b64encode(img.read()).decode()
+
+
+def df_to_base64(df):
+    """
+    Convert a Pandas DataFrame to a Base64 encoded CSV string.
+
+    Parameters:
+        df (pd.DataFrame): DataFrame to convert.
+
+    Returns:
+        str: Base64 encoded string of the DataFrame in CSV format.
+    """
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    return base64.b64encode(csv_buffer.getvalue().encode()).decode()
+
+
+def create_topic_df(topic, vectorizer, n_top_words=100):
+    """
+    Create a DataFrame containing the top words for a given topic.
+
+    Parameters:
+        topic (array): Array of word probabilities for a single topic.
+        vectorizer (CountVectorizer): The vectorizer used for text processing.
+        n_top_words (int, optional): Number of top words to include. Defaults to 100.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the top words and their probabilities.
+    """
+    normalized_topic = topic / topic.sum()
+    top_indices = normalized_topic.argsort()[-n_top_words:]
+    top_words = [
+        vectorizer.get_feature_names_out()[index] for index in reversed(top_indices)
+    ]
+    top_probs = [normalized_topic[index] for index in reversed(top_indices)]
+    return pd.DataFrame({"Word": top_words, "Probability": top_probs})
+
+
+def main():
+    """
+    Main function to run the Simple Topic Modeling app.
+    The app allows users to upload a corpus of text documents and discover topics within them using Latent Dirichlet Allocation (LDA).
+    It provides options for preprocessing the corpus, setting the model parameters, and visualizing the results.
+    """
+    # Set page config
+    st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+
+    # Sidebar
+    st.sidebar.title("About")
+    st.sidebar.markdown(
+        """
+        This app is a simple topic modeling tool that uses Latent Dirichlet Allocation (LDA) to discover topics in a corpus of text documents. 
+        It is based on the [scikit-learn](https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.LatentDirichletAllocation.html)
+        implementations of LDA and uses [pyLDAvis](https://github.com/bmabey/pyLDAvis) for visualizing the topics.
+        """
+    )
+    st.sidebar.info(
+        """
+        This app is maintained by [Moritz Mähr](https://maehr.github.io/).
+        """,
+        icon="ℹ️",
+    )
+
+    # Main page
+    st.title("Simple Topic Modeling")
+    st.markdown(
+        """
+        Topic modeling is a great way to discover the main themes in a corpus of text documents. 
+        It is an unsupervised learning technique that can be used to discover topics in a corpus of documents. 
+        Each topic is a distribution over the vocabulary of the corpus. 
+        The goal of topic modeling is to find a set of topics that best describes the corpus.
+        """
+    )
+    st.subheader("Step 1: Upload Your Text Files")
+    uploaded_files = st.file_uploader(
+        "Upload your text files containing the documents you want to discover topics for.",
+        type=["txt", "text", "md", "markdown", "rtf", "csv", "tsv", "log"],
+        accept_multiple_files=True,
+    )
+    if uploaded_files:
+        data = [
+            {"filename": file.name, "content": file.read().decode("utf-8")}
+            for file in uploaded_files
+        ]
+        df = pd.DataFrame(data)
+        st.markdown("**Corpus Statistics**")
+        st.write(f"Number of Documents: {df.shape[0]}")
+        st.write(
+            f"Average Document Length: {df['content'].apply(lambda x: len(x.split())).mean():.2f} words"
+        )
+        st.subheader("Step 2: Preprocessing the corpus")
+        st.markdown(
+            "Choose the preprocessing options that best suit your data. Removing stop words and short words can help improve the quality of the topics generated."
+        )
+        remove_stop_words = st.checkbox("Remove Stop Words", value=True)
+        if remove_stop_words:
+            language = st.selectbox("Choose Language for Stop Words", STOP_WORDS.keys())
+            use_custom_stop_words = st.checkbox("Use a Custom Stop Words List")
+            custom_stop_words = []
+
+        remove_short_words_and_numbers = st.checkbox(
+            "Remove Short Words and Numbers", value=True
+        )
+        if remove_stop_words and use_custom_stop_words:
+            custom_stop_words = [
+                word.strip()
+                for word in st.text_area("Enter Custom Stop Words").split(",")
+            ]
+        st.markdown(
+            """
+            Choose the n-gram range for the vectorizer. 
+            The n-gram range determines the number of words that are considered as a single token. 
+            For example, a unigram range means that each word is considered as a single token. 
+            A bigram range means that each pair of words is considered as a single token. 
+            A trigram range means that each triplet of words is considered as a single token.
+            """
+        )
+        ngram = st.selectbox("N-Gram Range", list(NGRAM_OPTIONS.keys()))
+        ngram_range = NGRAM_OPTIONS[ngram]
+        st.subheader("Step 3: Setting the model parameters")
+        st.markdown(
+            """
+            Choose the number of topics and the maximum number of iterations for the model.
+            The more iterations, the better the model will fit the data. But it will also take longer to run.
+            """
+        )
+        num_topics = st.slider("Number of Topics", 1, 20, 5)
+        max_iter = st.slider("Max Iterations", 10, 500, 50)
+        st.subheader("Step 4: Run the topic model and visualize the results")
+        st.markdown(
+            "Click the button below to run the topic model and discover topics in your corpus. This may take a while depending on the number of documents and the number of topics."
+        )
+        if st.button("Compute topic model"):
+            st.text("Processing...")
+            token_pattern = (
+                r"(?u)\b[a-zA-Z][a-zA-Z0-9_]{2,}\b"
+                if remove_short_words_and_numbers
+                else None
+            )
+            vectorizer = CountVectorizer(
+                lowercase=True,
+                stop_words=custom_stop_words
+                if use_custom_stop_words
+                else STOP_WORDS[language]
+                if remove_stop_words
+                else None,
+                token_pattern=token_pattern,
+                ngram_range=ngram_range,
+            )
+            dtm = vectorizer.fit_transform(df["content"])
+            lda = LatentDirichletAllocation(n_components=num_topics, max_iter=max_iter)
+            lda_output = lda.fit_transform(dtm)
+            dominant_topic = lda_output.argmax(axis=1)
+            topic_weights = lda_output.max(axis=1)
+            df_topic_weights = pd.DataFrame(
+                {"Dominant_Topic": dominant_topic, "Topic_Weight": topic_weights}
+            )
+            df_topic_weights["Filename"] = df["filename"]
+            df_topic_distribution = pd.DataFrame(
+                {
+                    "Document_Index": df_topic_weights.index,
+                    "Filename": df_topic_weights["Filename"],
+                    "Dominant_Topic": df_topic_weights["Dominant_Topic"],
+                    "Topic_Weight": df_topic_weights["Topic_Weight"],
+                }
+            )
+            df_all_topic_weights = pd.DataFrame(
+                lda_output, columns=[i + 1 for i in range(lda_output.shape[1])]
+            )
+            df_all_topic_weights["Dominant_Topic"] = df_all_topic_weights.idxmax(axis=1)
+            df_all_topic_weights_reset = df_all_topic_weights.reset_index(drop=True)
+            df_filename_reset = df["filename"].reset_index(drop=True)
+            df_topic_distribution = pd.concat(
+                [df_filename_reset, df_all_topic_weights_reset], axis=1
+            )
+            cols = ["filename", "Dominant_Topic"] + [
+                col
+                for col in df_topic_distribution.columns
+                if col not in ["filename", "Dominant_Topic"]
+            ]
+            df_topic_distribution = df_topic_distribution[cols]
+
+            if not platform.system() == "Emscripten":
+                st.subheader("Topics")
+                prepared_pyLDAvis_data = pyLDAvis.lda_model.prepare(
+                    lda, dtm, vectorizer
+                )
+                pyLDAvis_html = pyLDAvis.prepared_data_to_html(prepared_pyLDAvis_data)
+                st.components.v1.html(
+                    pyLDAvis_html, width=1200, height=1000, scrolling=True
+                )
+
+            st.subheader("Topic Distribution Over Documents")
+            st.dataframe(df_topic_distribution)
+
+            csv_str_base64 = df_to_base64(df_topic_distribution)
+            csv_href = f'<a href="data:file/csv;base64,{csv_str_base64}" download="topic_distribution_over_documents.csv">Download Topic Distribution Over Documents</a>'
+            st.markdown(csv_href, unsafe_allow_html=True)
+
+            for i, topic in enumerate(lda.components_):
+                st.subheader(f"Topic number {i+1}")
+                topic_df = create_topic_df(topic, vectorizer, n_top_words=100)
+                normalized_topic = topic / topic.sum()
+                top_indices = normalized_topic.argsort()[-10:]
+                top_words = [
+                    vectorizer.get_feature_names_out()[index] for index in top_indices
+                ]
+                top_probs = [normalized_topic[index] for index in top_indices]
+                # Bar Chart
+                fig, ax = plt.subplots(figsize=(10, 5))
+                ax.barh(top_words, top_probs)
+                ax.set_xlabel("Probability")
+                ax.set_title(f"Top Words for Topic {i+1}")
+                st.pyplot(fig)
+                barchart_base64 = fig_to_base64(fig)
+                barchart_href = f'<a href="data:image/png;base64,{barchart_base64}" download="barchart_topic_{i+1}.png">Download Bar Chart for Topic {i+1}</a>'
+                st.markdown(barchart_href, unsafe_allow_html=True)
+                plt.close(fig)
+                # Download top 100 words for the topic as CSV
+                topic_csv_base64 = df_to_base64(topic_df)
+                topic_csv_href = f'<a href="data:file/csv;base64,{topic_csv_base64}" download="top_words_topic_{i+1}.csv">Download Top 100 Words for Topic {i+1}</a>'
+                st.markdown(topic_csv_href, unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
